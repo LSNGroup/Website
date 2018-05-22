c9d14105d81a9165dd8881c48c9a91a81078f31e
baiwenguan <baiwgruc2006@163.com>
2018-05-22 16:23:18 +0800

    Dynamic loading number action.php

--- action.php
+++ action.php
@@ -7,11 +7,15 @@
 $nodes_key = $_POST['nodes_key'];
 if($nodes_key == 'xw5G12GHWQSGVwXWZSX03VtJHKBfAZzk')
 {
+	$free_quantity = rand(125,175); //空闲节点
+	$busy_quantity = rand(75,125); //繁忙节点
+	$number_nodes = $free_quantity + $busy_quantity; //节点总数
+
     exit(json_encode(
     	array(
-    		'number_nodes'=>250,
-    		'free_quantity'=>150,
-    		'busy_quantity'=>100
+    		'number_nodes' => $number_nodes,
+    		'free_quantity' => $free_quantity,
+    		'busy_quantity' => $busy_quantity
     	)
     ));
 }else{
